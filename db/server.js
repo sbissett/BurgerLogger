bcabf582cd7e2bb8d244a4d06332b4b6e75b4f69
Stephen Bissett <steve.bissett5@gmail.com>
2018-11-30 15:14:19 -0700

    server

---
+++ db/server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// bodyparsers 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+app.use(bodyParser.text({ type: 'text/html' }));
+
+// override with POST having ?_method=DELETE or PUT
+app.use(methodOverride('_method'));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
