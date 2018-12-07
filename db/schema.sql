7559d5d844e4d0e22613523ab1e3f2b66fa5ba5e
Stephen Bissett <steve.bissett5@gmail.com>
2018-12-06 18:25:36 -0700

    AUTO INT

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 CREATE TABLE burgers (
-    id INT NOT NULL,
+    id INT AUTO_INT,
     burger_name VARCHAR(200) NOT NULL,
     devoured BOOLEAN default FALSE,
     date TIMESTAMP,
